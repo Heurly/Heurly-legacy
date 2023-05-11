035d695364bd034fc2993f57be4681cb6a23855f
RaphBhz <raphael.bouchez@edu.esiee.fr>
2023-05-11 14:14:57 +0200

    Navbar and 2 subpages

---
+++ src/app/edt/page.tsx
@@ -0,0 +1,5 @@
+export default function Edt(): React.ReactElement {
+    return (
+        <p>This is the edt.</p>
+    )
+}
---
+++ src/app/hub/page.tsx
@@ -0,0 +1,7 @@
+import '../globals.css'
+
+export default function Hub(): React.ReactElement {
+  return (
+    <p>This is the hub.</p>
+  )
+}
