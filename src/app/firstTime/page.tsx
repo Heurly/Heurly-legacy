28fe1e50ba7791bcacaa28911e646109eb7705a8
domesday <m.andriamiraho@gmail.com>
2023-05-23 14:50:52 +0200

    fix: escape quote character

--- src/app/firstTime/page.tsx
+++ src/app/firstTime/page.tsx
@@ -6,7 +6,7 @@ export default function FirstTime() {
       <div className="bg-black w-screen h-[50vh] absolute bottom-0 -z-20  items-start justify-center">
         <div className=" -mt-20 flex flex-col items-center justify-center gap-y-5">
           <h1 className="text-3xl md:text-4xl font-black text-center text-black">
-            C'est la première fois ?
+            C&apos;est la première fois ?
           </h1>
           <div className="border-black border-4 rounded-xl flex bg-white pr-3 md:w-96">
             <input
