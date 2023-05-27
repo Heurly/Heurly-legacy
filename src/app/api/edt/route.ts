be0e1cb470fe36642be53af48480a0aec17cf822
RaphBhz <raphael.bouchez@edu.esiee.fr>
2023-05-27 17:56:31 +0200

    switched from ical2json to icalts

--- src/app/api/edt/route.ts
+++ src/app/api/edt/route.ts
@@ -4,8 +4,7 @@ import { PLANIF_ENDPOINT } from "@/app/api/ApiHelper";
 import { SUMMARY_MAP } from "@/course-config.json"
 import { CourseEvent } from "@/app/(layoutNavBar)/edt/types";
 import { NextRequest } from "next/server";
-// @ts-ignore
-import ical2json from "ical2json";
+import { lines2tree } from 'icalts'
 
 interface CalendarData {
   VCALENDAR: [
@@ -40,7 +39,7 @@ export async function GET(request: NextRequest) {
   }
 
   const VCALENDAR = await response.text();
-  var res: CalendarData = ical2json.convert(VCALENDAR);
+  var res: CalendarData = lines2tree(VCALENDAR.split("\r\n")) as unknown as CalendarData;
   res.VCALENDAR[0].VEVENT.forEach((course) => {
     const [subject, type]: string[] = course.SUMMARY.split(":")
     course.SUMMARY = `${SUMMARY_MAP[subject as keyof typeof SUMMARY_MAP] ?? subject} : ${type}`;
