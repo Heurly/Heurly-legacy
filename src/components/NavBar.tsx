75713e86918ef7bb03a8bdf63b630236a92c3a09
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2023-09-18 13:30:29 +0200

    feat: bold on page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,21 +5,43 @@ import { headers } from "next/headers";
 
 export default function NavBar() {
   const headersList = headers();
-  // read the custom x-url header
+
   const header_url = headersList.get("x-url") || "";
 
-  console.log(header_url)
   return (
     <nav className="flex md:flex-col items-center justify-between md:h-full md:w-1/5 w-full m-0 bg-neutral-950 border-r border-neutral-600 text-white fixed top-0 gap-y-5 max-w-sm min-w-[250px] overflow-auto">
       <div className="py-20 flex flex-col gap-y-10 text-xl">
         <Logo />
-        <Link href="#" className={header_url == "/dashboard" ? "font-bold" : ""}>
+        <Link
+          href="#"
+          className={header_url.includes("/dashboard") ? "font-bold" : ""}
+        >
           Dashboard
         </Link>
-        <Link href="#">Emplois du temps</Link>
-        <Link href="#">Actualités</Link>
-        <Link href="#">Ressources</Link>
-        <Link href="#">Forum</Link>
+        <Link
+          href="#"
+          className={header_url.includes("/edt") ? "font-bold" : ""}
+        >
+          Emplois du temps
+        </Link>
+        <Link
+          href="#"
+          className={header_url.includes("/actu") ? "font-bold" : ""}
+        >
+          Actualités
+        </Link>
+        <Link
+          href="#"
+          className={header_url.includes("/ressources") ? "font-bold" : ""}
+        >
+          Ressources
+        </Link>
+        <Link
+          href="#"
+          className={header_url.includes("/forum") ? "font-bold" : ""}
+        >
+          Forum
+        </Link>
       </div>
       <UserBar />
     </nav>
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,10 @@
 import cn from "classnames";
+import Image from "next/image";
 import Link from "next/link";
 export default function Logo(props: {className?: string}){
     return(
         <Link href="/dashboard">
-            <div className={cn(props.className)}>
-                <p className="font-bold text-3xl">
-                    ESIEE&nbsp;<span className="bg-white rounded px-1 py-1 text-black">HUB</span>
-                </p>
-            </div>
+            <Image src="./images/logo/logo_white.svg" width={200} height={200} alt="ESIEE hub logo"/>
         </Link>
     )
 }
