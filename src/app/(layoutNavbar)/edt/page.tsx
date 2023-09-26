a303494ea3e993268ad7e75f3d5f3dee1c74be45
RaphBhz <raphael.bouchez@etu.u-paris.fr>
2023-09-26 23:47:06 +0200

    wip form

--- src/app/(layoutNavbar)/edt/page.tsx
+++ src/app/(layoutNavbar)/edt/page.tsx
@@ -1,12 +1,14 @@
+'use client'
 import EDTForm from "@/components/edt/EDTForm";
 import CalendarElements from "@/components/edt/CalendarElements";
 import Grid from "@/components/edt/Calendar/Grid";
-import { CourseEvent } from "./types";
+import {CourseEvent, ModuleChoice} from "./types";
 import { API_URL } from "@/config/const";
+import {useEffect, useState} from "react";
 
 export const dynamic = "force-dynamic";
 
-async function getEDTData(): Promise<CourseEvent[]> {
+async function getEDTData(modules: ModuleChoice[]): Promise<CourseEvent[]> {
   const data = await fetch(API_URL + "/edt");
   try {
     const resp = await data.json();
@@ -20,10 +22,12 @@ async function getEDTData(): Promise<CourseEvent[]> {
 }
 
 export default async function Edt(): Promise<React.ReactElement> {
-  const edt = await getEDTData();
+  const [modules, setModules] = useState<ModuleChoice[]>([]);
+  const edt = await getEDTData(modules);
+
   return (
     <>
-      <EDTForm></EDTForm>
+      <EDTForm modules={modules} setModules={setModules}></EDTForm>
       <div className="relative w-full h-full text-center">
         <Grid />
         <CalendarElements edtData={edt} />
--- src/components/edt/EDTForm.tsx
+++ src/components/edt/EDTForm.tsx
@@ -1,12 +1,60 @@
 "use client";
-import React, { useState } from "react";
+import React, {Dispatch, SetStateAction, useState} from "react";
 import { CLASS_VALUES } from "@/app/(layoutNavbar)/edt/const";
-import ModuleInput from "@/components/edt/ModuleInput";
+import {ModuleChoice} from "@/app/(layoutNavbar)/edt/types";
+import Button from "@/components/Button";
 
-export default function EDTForm(): React.ReactElement {
+interface Props {
+  modules: ModuleChoice[];
+  setModules: Dispatch<SetStateAction<ModuleChoice[]>>;
+}
+
+const EDTForm: React.FunctionComponent<Props> = ({modules, setModules}) => {
   const [classValue, setClassValue] = useState(CLASS_VALUES.E3FI.value);
 
+  const addModule = (module: ModuleChoice) => {
+    if (module == undefined) return;
+
+    const newModules = modules.concat([module]);
+    setModules(newModules);
+  }
+  const removeModule = (module: string) => {
+    if (module == undefined) return;
+
+    const newModules = modules.filter(m => m.label != module);
+    setModules(newModules);
+  }
+
   return (
-      <ModuleInput data={[]} ></ModuleInput>
+      <div className="flex-col w-fullpb-4">
+        <div className="flex-row">
+          <input type="text" name="product" list="productName" />
+          <datalist
+              id="productName"
+              onInput={(event) =>
+                  addModule({label: event.currentTarget.innerText, code: parseInt(event.currentTarget.id)})
+              }>
+            <option value="Pen">Pen</option>
+            <option value="Pencil">Pencil</option>
+            <option value="Paper">Paper</option>
+          </datalist>
+          <Button className="text-white">Ajouter</Button>
+        </div>
+        <div className="flex flex-row p-2 text-white">
+          {modules.length >= 0 && modules.map(m => (
+              <>
+                <div
+                    onClick={(event) => removeModule(event.currentTarget.innerText)}
+                    className="h:cursor-pointer p-2"
+                >
+                  {m.label}
+                </div>
+              </>
+          ))}
+          {modules.length == 0 && (<div>Aucun module sélectionné</div>)}
+        </div>
+      </div>
   );
 }
+
+export default EDTForm;
