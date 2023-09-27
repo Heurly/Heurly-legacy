67a723939a695a3607bc49f741d13b1b625ea70a
domesday <m.andriamiraho@gmail.com>
2023-09-27 09:20:53 +0200

    style: update the bg color

--- src/app/(layoutNavbar)/layout.tsx
+++ src/app/(layoutNavbar)/layout.tsx
@@ -20,7 +20,7 @@ export default function NavbarLayout({
     <>
       <div className={cn(inter.className)}>
         <NavBar/>
-        <div className="md:w-4/5 md:ml-[20%] pt-[20%] md:p-5 bg-neutral-900">
+        <div className="md:w-4/5 md:ml-[20%] pt-[20%] md:p-5 bg-neutral-950">
           {children}
         </div>
       </div>
