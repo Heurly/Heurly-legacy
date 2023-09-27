9528018a5aa1cf87efbc83633d1e14af4f1cd35d
domesday <m.andriamiraho@gmail.com>
2023-09-27 09:54:57 +0200

    wip: ressource component

--- src/components/resource.tsx
+++ src/components/resource.tsx
@@ -9,11 +9,24 @@ export default function Resource({
   description: string;
 }): React.ReactElement {
   return (
-    <div className="rounded-xl bg-neutral-700 border-2 border-neutral-600 grid grid-rows-[1fr_40px] cursor-pointer aspect-square w-48 md:w-72 justify-self-center">
-      <div className="flex items-center justify-center">
-        <Image src={src} alt="image de title" width={50} height={50} />
+    <div className="flex flex-col text-white items-center">
+      <div className="p-5 bg-neutral-800 rounded-xl flex items-center justify-center aspect-square w-2/3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+          />
+        </svg>
       </div>
-      <div className="text-center text-white p-3 w-full">{title}</div>
+      test
     </div>
   );
 }
