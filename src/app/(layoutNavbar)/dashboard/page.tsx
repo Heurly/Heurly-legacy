e189e630a3f8f47d22d084ffb97916c3da572294
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2023-09-29 11:28:36 +0200

    update place card

--- src/app/(layoutNavbar)/dashboard/page.tsx
+++ src/app/(layoutNavbar)/dashboard/page.tsx
@@ -1,59 +1,77 @@
 import Actu from "@/components/Actu";
 import Card from "@/components/Card";
 
-
 export default async function Dashboard(): Promise<React.ReactElement> {
   const styles = {
-    marginTop: '0px',
+    marginTop: "0px",
     //marginBottom: '20px',
     //lineHeight: 4,
   };
 
-    const styles_font= {
-    color: 'rgb(75 85 99)',
+  const styles_date = {
+    color: "rgb(75 85 99)",
     //lineHeight: 4,
   };
 
   const styles_nextcours = {
-    marginLeft: '20px',
-
+    marginLeft: "20px",
   };
 
-  return (<div >
-    <div class="grid gap-4 grid-flow-col grid-rows-3">
-    
-    <Card><p style={styles}>
-      Actualité du moment :<br />
-    </p>
-    <Actu><div class="flex justify-between">
-      <p style={styles}>
-      Afterwork </p> 
-      <p style={styles_font}>date</p></div>
-      trop bien sous titre<br />
-      </Actu>
-      <Actu><div class="flex justify-between">
-      <p style={styles}>
-      Afterwork </p> 
-      <p style={styles_font}>date</p></div>
-      trop bien sous titre<br />
-      </Actu>
-      <Actu><div class="flex justify-between">
-      <p style={styles}>
-      Afterwork </p> 
-      <p style={styles_font}>date</p></div>
-      trop bien sous titre<br />
-      </Actu>
+  const styles_font = {
+    color: "rgb(163 163 163)",
+  };
 
-    </Card>
-    
-  <Card>Dernières ressources uploadées :<br/>
-    voilà</Card>
+  return (
+    <div className="grid grid-rows-2 grid-flow-col gap-4">
+      <div className="col-span-2">
+        <Card>
+          <div className="font-extrabold">
+            <p style={styles}>
+              Actualité du moment :<br />
+            </p>
+          </div>
+          <div className="divide-y divide-gray-500 md:divide-y-2">
+            <Actu>
+              <div className="flex justify-between font-bold">
+                <p style={styles}>Afterwork </p>
+                <p style={styles_date}>date</p>
+              </div>
+              <p style={styles_font}>trop bien sous titre</p>
+            </Actu>
+            <div className="">
+              <Actu>
+                <div className="flex justify-between font-bold">
+                  <p style={styles}>Afterwork </p>
+                  <p style={styles_date}>date</p>
+                </div>
+                <p style={styles_font}>trop bien sous titre</p>
+              </Actu>
+            </div>
+            <Actu>
+              <div className="flex justify-between font-bold">
+                <p style={styles}>Afterwork </p>
+                <p style={styles_date}>date</p>
+              </div>
+              <p style={styles_font}>trop bien sous titre</p>
+            </Actu>
+          </div>
+        </Card>
+      </div>
+      <div className="col-span-2">
+      <Card>
+        <div className=" row-span-2 col-span-2 font-extrabold">
+          Dernières ressources uploadées :<br />
+        </div>
+        voilà
+      </Card>
+      </div>
 
-    <div class = "row-span-3 ">
-   <Card>Prochain cours :<br/>
-   test</Card>
-   </div>
+      <div className=" flex items-stretch row-span-2 col-span-1 font-extrabold self-stretch">
+        <Card>
+          Prochain cours :<br />
+          test
+        </Card>
+      </div>
     </div>
-  </div>
   );
 }
--- src/components/Actu.tsx
+++ src/components/Actu.tsx
@@ -2,7 +2,7 @@
 
 export default async function Actu({children}): Promise<React.ReactElement> {
 
-    return (<div className="border-b-2 border-zinc-700 bg-neutral-950 text-white p-3 ">
+    return (<div className=" bg-neutral-950 text-white p-3 ">
       {children}
     </div>
     );
