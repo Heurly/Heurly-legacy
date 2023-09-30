669d573909915a2bdbd3752452da173a36aabd5c
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2023-09-30 13:21:10 +0200

    print values

--- create_subdomain.py
+++ create_subdomain.py
@@ -15,6 +15,13 @@ client = ovh.Client(
 domain = os.environ['OVH_DOMAIN']
 subdomain = os.environ['OVH_SUBDOMAIN']
 
+print("OVH_APP_KEY:", application_key)
+print("OVH_APP_SECRET:", application_secret)
+print("OVH_CONSUMER_KEY:", os.environ['OVH_CONSUMER_KEY'])
+print("OVH_DOMAIN:", domain)
+print("OVH_SUBDOMAIN:", subdomain)
+
+
 client.post('/domain/zone/{0}/record'.format(domain),
     fieldType='A',
     subDomain=subdomain,
