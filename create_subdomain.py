8c9bcb92233d7f87bd726afcc5fb77bfb30beac0
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2023-09-30 18:05:54 +0200

    Test subdomain creation

--- create_subdomain.py
+++ create_subdomain.py
@@ -7,25 +7,18 @@ consumer_key = os.environ['OVH_CONSUMER_KEY']
 
 domain = os.environ['OVH_DOMAIN']
 subdomain = os.environ['OVH_SUBDOMAIN']
-
-
-print(application_key)
-print(application_secret)
-print(consumer_key)
-print(domain)
-print(subdomain)
-
+endpoint = os.environ['OVH_ENDPOINT']
 
 # Initialize the OVH client
-# client = ovh.Client(
-#     endpoint=os.environ['OVH_ENDPOINT'],
-#     application_key=os.environ['OVH_APP_KEY'],
-#     application_secret=os.environ['OVH_APP_SECRET'],
-#     consumer_key=os.environ['OVH_CONSUMER_KEY']
-# )
+client = ovh.Client(
+    endpoint=endpoint,
+    application_key=application_key,
+    application_secret=application_secret,
+    consumer_key=consumer_key
+)
 
-# client.post('/domain/zone/{0}/record'.format(domain),
-#                 fieldType='A',
-#                 subDomain=subdomain,
-#                 target='82.64.216.184',
-#                 ttl=3600)
+client.post('/domain/zone/{0}/record'.format(domain),
+                fieldType='A',
+                subDomain=subdomain,
+                target='82.64.216.184',
+                ttl=3600)
