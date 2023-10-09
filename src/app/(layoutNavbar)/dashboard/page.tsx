d13c8fc7f032be75de0082cbbe8773ce1310d493
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2023-10-09 09:06:05 +0200

    feat/dashboard : push pour recup le code

--- src/app/(layoutNavbar)/dashboard/page.tsx
+++ src/app/(layoutNavbar)/dashboard/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 import Actu from "@/components/Actu";
 import Card from "@/components/Card";
+import Resource from "@/components/resource";
+import { SearchContext } from "@/context/search";
+import { useContext } from "react";
 
 export default async function Dashboard(): Promise<React.ReactElement> {
   const styles = {
@@ -20,6 +24,25 @@ export default async function Dashboard(): Promise<React.ReactElement> {
   const styles_font = {
     color: "rgb(163 163 163)",
   };
+
+  const { search } = useContext(SearchContext);
+
+  const resources = [
+    {
+      title: "test",
+      description: "test",
+      date: "2021-10-10",
+    },
+    {
+      title: "lol",
+      description: "caca",
+      date: "2021-10-10",
+    },
+    {
+      title: "bla",
+      description: "ohhhh",
+      date: "2021-10-10",
+    }];
 
   return (
     <div className="grid grid-rows-2 grid-flow-col gap-4">
@@ -62,14 +85,35 @@ export default async function Dashboard(): Promise<React.ReactElement> {
           <div className=" row-span-2 col-span-2 font-extrabold">
             Dernières ressources uploadées :<br />
           </div>
+          {/*<div className="p-5 grid md:grid-cols-4 grid-cols-2 gap-4">
+            {resources &&
+              resources
+                .filter(
+                  (resource) =>
+                    resource.title.includes(search) ||
+                    resource.description.includes(search)
+                )
+                .map((resource, key) => (
+                  <Resource
+                    key={key}
+                    title={resource.title}
+                    description={resource.description}
+                  />
+                ))}
+                </div>*/}
           voilà
         </Card>
       </div>
 
-      <div className="  grid justify-items-stretch flex items-stretch row-span-2 col-span-1 font-extrabold self-stretch justify-self-strech">
+      <div className="  grid justify-items-stretch flex items-stretch row-span-2 col-span-1 font-extrabold self-stretch justify-self-strech ">
         <Card>
           Prochain cours :<br />
+          <div className="divide-y divide-gray-500 md:divide-y-2">
+          <div className="z-10">
           8h
+          10h
+          </div>
+          </div>
           <Card>Mathématiques 2h</Card>
           10h
           <Card>Réseaux 2h</Card>
@@ -80,6 +124,7 @@ export default async function Dashboard(): Promise<React.ReactElement> {
           15h
           <Card>Machine Learning 2h</Card>
           17h
+          
         </Card>
       </div>
     </div>
