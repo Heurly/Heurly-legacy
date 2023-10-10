d4a748a8fae707d253568d9e053a1ba46af6c4c6
RaphBhz <raphael.bouchez@edu.esiee.fr>
2023-10-10 17:16:01 +0200

    delete logout route

--- src/app/api/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import {NextRequest, NextResponse} from "next/server";
-import {ROOT_URL} from "@/config/const";
-
-export async function GET(request: NextRequest) {
-    const response = NextResponse.redirect(`${ROOT_URL}/`);
-    response.cookies.delete('user');
-    return response;
-}
