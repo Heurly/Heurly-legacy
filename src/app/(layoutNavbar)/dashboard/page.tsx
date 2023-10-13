c1934a3e3e15ff8a0bb118ce8046868fdbf1e687
agiordanoge1 <angelo.giordano@universite-paris-saclay.fr>
2023-10-13 11:47:49 +0200

    feat/dashboard : resize of the dashboard

--- src/app/(layoutNavbar)/dashboard/page.tsx
+++ src/app/(layoutNavbar)/dashboard/page.tsx
@@ -27,7 +27,7 @@ export default async function Dashboard(): Promise<React.ReactElement> {
 
   const ressources_upload = [
     {
-      title: "tes",
+      title: "test",
       description: "early ou quoi",
       date: "2021-10-10",
     },
@@ -44,47 +44,42 @@ export default async function Dashboard(): Promise<React.ReactElement> {
   ];
 
   return (
-    <div className=" grid grid-rows-2 grid-flow-col gap-4">
-      <div className="z-20 col-span-2">
-        <Card>
-          <div className="font-extrabold">
-            <p style={styles}>
-              Actualité du moment :<br />
-            </p>
-          </div>
-          <div className="divide-y divide-gray-500 md:divide-y-2">
-            <Actu>
-              <div className="flex justify-between font-bold">
-                <p style={styles}>Afterwork </p>
-                <p style={styles_date}>date</p>
-              </div>
-              <p style={styles_font}>trop bien sous titre</p>
-            </Actu>
-            <div className="">
-              <Actu>
-                <div className="flex justify-between font-bold">
-                  <p style={styles}>Afterwork </p>
-                  <p style={styles_date}>date</p>
-                </div>
-                <p style={styles_font}>trop bien sous titre</p>
-              </Actu>
+    <div className="grid grid-rows-2 grid-flow-col gap-4 h-[700px] ">
+      <Card className="z-20 col-span-2">
+        <div className="font-extrabold mb-6 ">
+          <p style={styles}>Actualité du moment :</p>
+        </div>
+        <div className="divide-y divide-gray-500 md:divide-y-2">
+          <Actu>
+            <div className="flex justify-between font-bold">
+              <p style={styles}>Afterwork </p>
+              <p style={styles_date}>date</p>
             </div>
-            <Actu>
-              <div className="flex justify-between font-bold">
-                <p style={styles}>Afterwork </p>
-                <p style={styles_date}>date</p>
-              </div>
-              <p style={styles_font}>trop bien sous titre</p>
-            </Actu>
-          </div>
-        </Card>
-      </div>
-      <div className="col-span-2">
-        <Card>
-          <div className=" row-span-2 col-span-2 font-extrabold">
+            <p style={styles_font}>trop bien sous titre</p>
+          </Actu>
+          <Actu>
+            <div className="flex justify-between font-bold">
+              <p style={styles}>Afterwork </p>
+              <p style={styles_date}>date</p>
+            </div>
+            <p style={styles_font}>trop bien sous titre</p>
+          </Actu>
+          <Actu>
+            <div className="flex justify-between font-bold">
+              <p style={styles}>Afterwork </p>
+              <p style={styles_date}>date</p>
+            </div>
+            <p style={styles_font}>trop bien sous titre</p>
+          </Actu>
+        </div>
+      </Card>
+
+      <div className="col-span-2 flex items-stretch">
+        <Card className=" self-stretch flex-auto">
+          <div className=" row-span-2 font-extrabold mb-8 mt-4">
             Dernières ressources uploadées :<br />
           </div>
-          <div className="p-5 grid md:grid-cols-4 grid-cols-2 gap-4">
+          <div className="p-8 grid md:grid-cols-4 gap-4 place-content-center">
             {ressources_upload &&
               ressources_upload.map((resource, key) => (
                 <Resource
@@ -97,76 +92,74 @@ export default async function Dashboard(): Promise<React.ReactElement> {
         </Card>
       </div>
 
-      <div className="z-20 grid justify-items-stretch flex items-stretch row-span-2 col-span-1 font-extrabold self-stretch justify-self-strech ">
-        <Card>
-          Prochain cours :
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
-              8h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
-              9h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
-              10h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
-              11h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              12h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              13h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              14h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              15h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              16h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              17h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              18h
-            </span>
-          </div>
-          <div className="border-t relative h-5 mt-10">
-            <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
-              19h
-            </span>
-          </div>
-          <div className="border-t relative mt-10">
-            <span className="absolute top-[-20px] left-[10px] py-2 px-3 bg-neutral-950">
-              20h
-            </span>
-          </div>
-        </Card>
-      </div>
+      <Card className="overflow-auto z-20 grid justify-items-stretch row-span-2 col-span-1 font-extrabold self-stretch justify-self-strech ">
+        Prochain cours :
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
+            8h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
+            9h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
+            10h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-25px] left-[10px] py-2 px-3 bg-neutral-950">
+            11h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            12h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            13h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            14h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            15h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            16h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            17h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            18h
+          </span>
+        </div>
+        <div className="border-t relative h-5 mt-10">
+          <span className="absolute top-[-30px] left-[10px] py-5 px-3 bg-neutral-950">
+            19h
+          </span>
+        </div>
+        <div className="border-t relative mt-10">
+          <span className="absolute top-[-20px] left-[10px] py-2 px-3 bg-neutral-950">
+            20h
+          </span>
+        </div>
+      </Card>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,13 @@
+import cn from "classnames"
 export default function Card({
   children,
+  className
 }: {
   children: React.ReactNode;
+  className? : string
 }): React.ReactElement {
   return (
-    <div className="relative border border-neutral-600 bg-neutral-950 text-white rounded-[15px] p-7  ">
+    <div className={cn("relative border border-neutral-600 bg-neutral-950 text-white rounded-[15px] p-7  ", className)}>
       {children}
     </div>
   );
