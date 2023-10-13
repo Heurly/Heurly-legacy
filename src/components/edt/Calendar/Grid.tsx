a01eda43034e4c7064e051aab39ce29fa095b33b
domesday <m.andriamiraho@gmail.com>
2023-10-13 18:09:40 +0200

    feat: hook for dimension and responsive grid

--- src/components/edt/Calendar/Grid.tsx
+++ src/components/edt/Calendar/Grid.tsx
@@ -1,76 +1,31 @@
-import {
-    LINES,
-    COLUMNS,
-    DAYS, DAY_IN_MS,
-} from "@/app/(layoutNavbar)/edt/const";
+"use client";
 import id from "@/utils/id";
 import React from "react";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 interface Props {
-    date: Date;
+  date: Date;
 }
 
-export default function Grid({date}: Props): React.ReactElement {
+export default function Grid(): React.ReactElement {
   const grid = [];
-  const week = new Date(date.getTime() - (date.getDay() - 1) * DAY_IN_MS);
+  const { width } = useWindowDimensions();
+  const nbPxPhone = 768;
+  const nbCell = width > nbPxPhone ? 98 : 14;
 
-  for (let i = 0; i < LINES - 1; i++) {
-    // we put the days in the grid
-    if (i == 0) {
-      // we push the empty case
-      grid.push(
-        <div
-          key={`${id()}`}
-          className="w-full flex flex-col items-center justify-center text-white bg-neutral-950"
-          style={{
-            gridColumn: `${1} / span 1`,
-            gridRow: `${1} / span 1`,
-          }}
-        />
-      );
-    }
-    if (i <= DAYS.length) {
-      grid.push(
-        <div
-          key={`${id()}`}
-          className="w-full flex flex-col items-center justify-center text-white bg-neutral-950 font-bold"
-          style={{
-            gridColumn: `${i + 1} / span 1`,
-            gridRow: `${1} / span 1`,
-          }}
-        >
-          <div>{DAYS[i - 1]}</div>
-            <div className="text-neutral-600">{i != 0 &&
-                new Date(week.getTime() + (i-1)*DAY_IN_MS).toLocaleString('fr-FR', { month: 'numeric', day: 'numeric'})
-            }</div>
-        </div>
-      );
-    }
-    // we put the hours in the grid
-    if (i <= 13 && i != 0) {
-      grid.push(
-        <div
-          key={`${id()}`}
-          className="w-full flex flex-col items-center justify-end text-white bg-neutral-950 font-bold"
-          style={{
-            gridColumn: `${1} / span 1`,
-            gridRow: `${i + 1} / span 1`,
-          }}
-        >
-          <div className="relative -bottom-5 bg-neutral-950 p-2">{i + 7 == 13+7 ? '' : `${i + 7}h`}</div>
-        </div>
-      );
-    }
-
-    for (let j = 0; j < COLUMNS - 1; j++) {
-      grid.push(
-        <div key={`${id()}`} className="w-full h-full" />
-      );
-    }
+  for (let j = 0; j < nbCell; j++) {
+    grid.push(
+      <div
+        key={`${id()}`}
+        className="w-full h-20 md:h-14 border-t md:border border-neutral-600"
+      />
+    );
   }
 
   return (
-    <div className="grid grid-cols-7 grid-rows-14 w-full h-full rounded-xl overflow-hidden border border-neutral-800 divide-neutral-800 divide-x divide-y">
+    <div
+      className={`grid grid-cols-1 md:grid-cols-3 lg:grid-cols-7 w-full h-full overflow-hidden px-3 md:px-0`}
+    >
       {grid}
     </div>
   );
