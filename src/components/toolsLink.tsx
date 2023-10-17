5508f514bb6c9d54dcafc92e14d4600efc160e5d
qiaoqiao xia <xiaqiaoqiao0601@gmail.com>
2023-10-17 23:44:11 +0200

    first version hide and show navbar

--- src/components/toolsLink.tsx
+++ src/components/toolsLink.tsx
@@ -7,7 +7,7 @@ export default function ToolsLink() {
     const links = [
       { imageLink: "/images/webaurion.svg", text: "Webaurion",urlLink: "https://webaurion.esiee.fr/faces/Login.xhtml" },
       { imageLink: "/images/instraEsiee.svg", text: "Intranet ESIEE", urlLink: "https://intra.esiee.fr/" },
-      { imageLink: "/images/bdeInstra.svg", text: "BDE Instra", urlLink: "https://www.instagram.com/bde_esiee_paris/reels/" },
+      { imageLink: "/images/bdeInstra.svg", text: "BDE Insta", urlLink: "https://www.instagram.com/bde_esiee_paris/reels/" },
       { imageLink: "/images/blackboard.svg", text: "Blackboard",urlLink: "https://esiee.blackboard.com/webapps/login/" },
       
     ];
