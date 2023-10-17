71530bc1e4f33626e7b0be01a89e0c9b6a7b3da0
qiaoqiao xia <xiaqiaoqiao0601@gmail.com>
2023-10-17 23:44:11 +0200

    first version hide and show navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -27,15 +27,26 @@ const NavBar: React.FunctionComponent = () => {
     { imageLink: "/images/forum.svg", text: "Forum", urlLink: "/forum" },
   ];
 
-  return (
-    <nav className="flex flex-col items-center space-y-10 py-10 fixed top-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 dark: bg-neutral-950 border-r border-neutral-600 text-white">
+    return (
+      <>
+      <input className="peer" type="checkbox" id="navbarToggle" />
+      <label htmlFor="navbarToggle" className="z-10 absolute top-2 left-2 cursor-pointer w-8 h-8 p-1 hover:bg-neutral-600 rounded-lg">
+          <svg aria-hidden="true" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+          </svg>
+      </label>
+      
+      <nav className="flex flex-col items-center space-y-10 py-10 fixed top-0 w-64 h-full transition-transform -translate-x-full sm:translate-x-0 dark:bg-neutral-950 border-r border-neutral-600 text-white  peer-checked:hidden ">
       <div className="flex flex-col space-y-5 text-xl">
-        <div>
-          <Logo></Logo>
-        </div>
+          <div className="flex flex-row items-stretch ">
+              <div><Logo></Logo></div>
+                <div className="pl-10">
+                </div>
+          </div >
+
         {links.map((link, index) => (
-          <Link key={index} href={link.urlLink} as={link.urlLink}>
-            <div className={header_url.includes(link.urlLink) ? "" : ""}>
+            <Link key={index} href={link.urlLink} as={link.urlLink}>
+              <div className={header_url.includes(link.urlLink) ? "" : ""}>
               <div className="flex gap-x-3 p-1 rounded-lg hover:bg-neutral-600 transition duration-200 cursor-pointer">
                 <div className="flex items-center">
                   <Image
@@ -55,10 +66,11 @@ const NavBar: React.FunctionComponent = () => {
         ))}
       </div>
 
-      <ToolsLink />
-      <UserBar />
-    </nav>
-  );
-};
+        <ToolsLink/>
+        <UserBar/>
+      </nav>
+      </>
+    );
+}
 
 export default NavBar;
