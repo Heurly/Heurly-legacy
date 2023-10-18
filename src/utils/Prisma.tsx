776c8c105761bbe8e50be08d76cde31daa448a94
RaphBhz <raphael.bouchez@edu.esiee.fr>
2023-10-18 10:09:14 +0200

    fix prisma singleton

--- src/utils/Prisma.tsx
+++ src/utils/Prisma.tsx
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-export default prismaClient;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prsimaClient = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prsimaClient;
