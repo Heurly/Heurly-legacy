6d2e8575f9fe85c9822e137b3016797c33049559
qiaoqiao xia <xiaqiaoqiao0601@gmail.com>
2023-10-20 12:17:04 +0200

    session active

--- src/app/(layoutNavbar)/layout.tsx
+++ src/app/(layoutNavbar)/layout.tsx
@@ -23,15 +23,15 @@ export default async function NavbarLayout({
 
   return (
     <>
-      {/* {session && ( */}
+       {session && ( 
         <div className={cn(inter.className)}>
           <NavBar />
           <div className="md:w-4/5 md:ml-[20%] pt-[20%] md:p-5 bg-neutral-950">
             {children}
           </div>
         </div>
-      {/* )}
-      {!session && redirect("/login", RedirectType.replace)} */}
+       )}
+      {!session && redirect("/login", RedirectType.replace)} 
     </>
   );
 }
