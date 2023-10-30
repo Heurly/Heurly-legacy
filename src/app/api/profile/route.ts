dc8144d47fdddec8619580d74454affd594b5745
RaphBhz <raphael.bouchez@edu.esiee.fr>
2023-10-30 13:27:23 +0100

    pull prisma

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -9,15 +9,8 @@ export interface UpdateProfilePayload {
 export async function POST(request: NextRequest) {
   const payload = (await request.json()) as UpdateProfilePayload;
 
-  const prismaBullshit = await prismaClient.user.findFirst({
-    where: { email: payload.email },
-  });
-
-  if (prismaBullshit == undefined) return Response.error();
-
   const update = await prismaClient.user.update({
     where: {
-      id: prismaBullshit.id,
       email: payload.email,
     },
     data: {
